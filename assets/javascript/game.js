165f758f4a6b0ef2fda74a1b167b1d6f6eb16445
Krista Brehm <kristabrehm1@gmail.com>
2018-10-18 20:42:43 -0400

    switched it to my back up

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,8 +3,10 @@ var wins = 0;
 var losses = 0;
 var score = 0;
 
+
 function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (min - max + 1)) + min;
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 var targetNumber = getRandomInt(19, 120);
@@ -12,14 +14,20 @@ $("#number-to-guess").text(targetNumber);
 var counter = 0;
 
 for (var i = 0; i < 4; i++) {
-    var imageCrystal = $("<img></img>");
+    var imageCrystal = $("<img>");
     imageCrystal.addClass("crystal-image");
-    imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg")
-    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+    imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+    imageCrystal.attr("data-crystalvalue", getRandomInt(1, 12));
     $("#crystals").append("crystals");
 }
 
-$(".crystal-image").on("click", function() {
-    var crystalValue = ($(this).attr("data-crystalvalue"));
+$(".crystal-image").on("click", function () {
+    var crystalValue = parseInt($(this).attr("data-crystalvalue"));
+    counter += crystalValue;
+    alert("New score: " + counter);
+    if (counter === targetNumber) {
+        alert("You win!");
+    } else if (counter >= targetNumber) {
+        alert("You lose!!");
     }
 });
