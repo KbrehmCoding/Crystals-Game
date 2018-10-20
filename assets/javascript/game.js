0f910e748a2705e904ccbc342ae22664f3946ee7
Krista Brehm <kristabrehm1@gmail.com>
2018-10-19 22:11:51 -0400

    moved errant code so its all inside functions, changed code so it was all jquery/js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,48 @@
-
 var wins = 0;
 var losses = 0;
 var score = 0;
-var winsText = document.getElementById("wins-text");
-var lossesText = document.getElementById("losses-text");
-var scoreText = document.getElementById("score-text");
-
-function startNewGame() {
-    score = 0;
-    computerChoice = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-}
+var targetNumber = 0;
 
 function getRandomInt(min, max) {
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-var targetNumber = getRandomInt(19, 120);
-("#number-to-guess").text(targetNumber);
-var counter = 0;
-
-for (var i = 0; i < 4; i++) {
-    var imageCrystal = $("<img>");
-    imageCrystal.addClass("crystal-image");
-    imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
-    imageCrystal.attr("data-crystalvalue", getRandomInt(1, 12));
-    ("#crystals").append("crystals");
+function startNewGame() {
+    score = 0;
+    targetNumber = getRandomInt(19, 120);
+    $("#targetNumber-text").text(targetNumber);
+    createCrystals();
 }
 
-(".crystal-image").on("click", function () {
-    var crystalValue = parseInt($(this).attr("data-crystalvalue"));
-    counter += crystalValue;
-    alert("New score: " + counter);
-    if (counter === targetNumber) {
-        wins++;
-    } else if (counter > targetNumber) {
-        losses++;
+function createCrystals() {
+    $("#crystals").empty();
+    for (var i = 0; i < 4; i++) {
+        var imageCrystal = $("<img>");
+        imageCrystal.addClass("crystal-image");
+        imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+        imageCrystal.attr("data-crystalvalue", getRandomInt(1, 12));
+        $("#crystals").append(imageCrystal);
     }
+}
+
+function setCrystalsClickHandler() {
+    $("#crystals").on("click", ".crystal-image", function () {
+        var crystalValue = parseInt($(this).attr("data-crystalvalue"));
+        score += crystalValue;
+        if (score === targetNumber) {
+            wins++;
+        } else if (score > targetNumber) {
+            losses++;
+            startNewGame();
+        }
+        $("#wins-text").text(wins);
+        $("#losses-text").text(losses);
+        $("#score-text").text(score);
+    });
+}
+
+$(document).ready(function () {
+    setCrystalsClickHandler();
+    startNewGame();
 });
